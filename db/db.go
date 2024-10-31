d49d8e2c9258537593ec87264670dd2a6ac3bd22
Antonin Arsenault <aarsenault@korsr.studio>
2024-10-30 21:36:17 -0400

    Add fatal err to test

--- db/db.go
+++ db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"log"
 	"os"
 
 	"github.com/jackc/pgx/v5"
@@ -54,6 +55,7 @@ func Delete(name string) error {
 func List() (map[string]Url, error) {
 	rows, err := db.Query(context.Background(), "SELECT name, url FROM url")
 	if err != nil {
+		log.Fatal(err)
 		return nil, err
 	}
 	defer rows.Close()
